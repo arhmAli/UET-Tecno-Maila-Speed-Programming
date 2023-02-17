ab0aa85c88286079c72c0ab993f00aa72ac13c62
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-17 12:05:02 +0500

    solved task 1

---
+++ Task1.c
@@ -0,0 +1,13 @@
+// Initialize a variable at 3 and end its
+//value at before 16 show its output
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int sum=3;
+for(int i=sum;i<16;i=i+2){
+    cout<<"The value of the variable is"<<i<<endl;
+}
+}
