5b8fe02b7c1762bcc798ec303a00c3b269ffa2ca
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-17 12:16:46 +0500

    tried solving task2

---
+++ task2.c
@@ -0,0 +1,28 @@
+// Give an output of the following order
+//+++++
+//++++
+//+++
+//++
+//+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+char array2d[]={{}};
+for(int i=1;i<=6;i++){
+array2d[i]="*";
+for(int j=6;j<=1;j--){
+array2d[j]="*";    
+
+cout<<array2d;
+    
+}
+cout<<endl;
+}
+return 0;
+}
+
+return 0;
+}
